44886106866ce7c863242dedec2021bbe950d87b
Vivek-Cdac-2016 <54227039+Vivek-Cdac-2016@users.noreply.github.com>
2019-08-18 16:10:17 +0530

    Add files via upload

---
+++ accubit.sql
@@ -0,0 +1,19 @@
+//  This are the below Query to create database and table
+
+
+CREATE DATABASE accubite_test;
+CREATE TABLE registration_details
+(
+name varchar(12),
+email varchar(20),
+password varchar(20),
+
+);
+
+
+CREATE TABLE blog_detail
+(
+title varchar(50),
+blogdiscription varchar(200),
+
+);
